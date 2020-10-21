b6f2b0d7938007f6759e42050e1553eff586337b
Peet Gouws <peet@gouws.com>
2020-10-21 17:31:26 +0200

    Init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a class="active item" href="/">Image Storage</a>
+    <div class="right menu">
+      <a class="item"> Log In </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
